01ebbe4e7968731c422e33bd902e25271e8e3030
Коледа Наталья Валерьевна <natallia.kaliada@sc.softclub.by>
2020-07-06 18:58:58 +0300

    Output component template

--- src/components/excel/Excel.js
+++ src/components/excel/Excel.js
@@ -1,6 +1,24 @@
 export class Excel {
-    constructor(selector, options) {
-        this.$el = document.querySelector(selector);
-        this.components = options.components || [];
-    }
+	constructor(selector, options) {
+		this.$el = document.querySelector(selector);
+		this.components = options.components || [];
+	}
+
+	getRoot() {
+		const $root = document.createElement('div');
+		this.components.forEach(Component => {
+			const component = new Component();
+			$root.insertAdjacentHTML('beforeend', component.toHTML());
+		});
+		return $root;
+	}
+
+	render() {
+		// console.log(this.$el);
+		// this.$el.insertAdjacentHTML('afterbegin', `<h1>Test</h1>`);
+		// this.$el.append(document.createElement('h1'));
+		// const node = document.createElement('h1');
+		// node.textContent = 'test';
+		this.$el.append(this.getRoot());
+	}
 }
--- src/core/ExcelComponent.js
+++ src/core/ExcelComponent.js
@@ -1,9 +1,8 @@
-import {DomListener} from "@core/DomListener";
+import { DomListener } from '@core/DomListener';
 
 export class ExcelComponent extends DomListener {
-
-    // возвращает шаблон компонента
-    toHTML() {
-        return '';
-    }
+	// возвращает шаблон компонента
+	toHTML() {
+		return '';
+	}
 }
