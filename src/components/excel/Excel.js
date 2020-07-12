38b9bdf4f4fc0f373f5d29e19a18fd7618c79490
Natka <koleda.natallia@gmail.com>
2020-07-12 22:28:03 +0300

    Layout development for excel page

--- src/components/excel/Excel.js
+++ src/components/excel/Excel.js
@@ -1,24 +1,28 @@
+import {$} from '@core/dom';
+
 export class Excel {
-	constructor(selector, options) {
-		this.$el = document.querySelector(selector);
-		this.components = options.components || [];
-	}
+    constructor(selector, options) {
+        this.$el = document.querySelector(selector);
+        this.components = options.components || [];
+    }
 
-	getRoot() {
-		const $root = document.createElement('div');
-		this.components.forEach(Component => {
-			const component = new Component();
-			$root.insertAdjacentHTML('beforeend', component.toHTML());
-		});
-		return $root;
-	}
+    getRoot() {
+        const $root = $.create('div', 'excel');
+        this.components.forEach(Component => {
+            const $el = $.create('div', Component.className);
+            const component = new Component($el);
+            $el.innerHTML = component.toHTML();
+            $root.append($el);
+        });
+        return $root;
+    }
 
-	render() {
-		// console.log(this.$el);
-		// this.$el.insertAdjacentHTML('afterbegin', `<h1>Test</h1>`);
-		// this.$el.append(document.createElement('h1'));
-		// const node = document.createElement('h1');
-		// node.textContent = 'test';
-		this.$el.append(this.getRoot());
-	}
+    render() {
+        // console.log(this.$el);
+        // this.$el.insertAdjacentHTML('afterbegin', `<h1>Test</h1>`);
+        // this.$el.append(document.createElement('h1'));
+        // const node = document.createElement('h1');
+        // node.textContent = 'test';
+        this.$el.append(this.getRoot());
+    }
 }
