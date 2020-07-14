fb3ed2b16f07c4f3b52d9df68563cda8155ea8fd
Коледа Наталья Валерьевна <natallia.kaliada@sc.softclub.by>
2020-07-14 16:56:31 +0300

    Adding Event Listening

--- src/core/DomListener.js
+++ src/core/DomListener.js
@@ -1,8 +1,35 @@
+import { capitalize } from '@core/utils';
+
 export class DomListener {
-    constructor($root) {
-        if (!$root) {
-            throw new Error(`No root provided for DomListener`);
-        }
-        this.$root = $root;
-    }
+	constructor($root, listeners = []) {
+		if (!$root) {
+			throw new Error(`No root provided for DomListener`);
+		}
+		this.$root = $root;
+		this.listeners = listeners;
+	}
+
+	initDOMListeners() {
+		this.listeners.forEach(listener => {
+			const method = getMethodName(listener);
+			// console.log(method);
+			// console.log(this);
+			// console.log(this.$root);
+			if (!this[method]) {
+				const name = this.name || '';
+				throw new Error(
+					`Method ${method} is not implemented in ${name} Component`);
+			}
+			this.$root.on(listener, this[method].bind(this));
+		});
+	}
+
+	removeDOMListeners() {
+
+	}
+}
+
+// input => onInput
+function getMethodName(eventName) {
+	return 'on' + capitalize(eventName);
 }
