5b84041ebd029fe241f146ae77bf5101d6322546
Коледа Наталья Валерьевна <natallia.kaliada@sc.softclub.by>
2020-11-04 16:06:38 +0300

    Creating router

--- src/index.js
+++ src/index.js
@@ -1,27 +1,32 @@
 import './scss/index.scss';
-import { Excel } from '@/components/excel/Excel';
-import { Header } from '@/components/header/Header';
-import { Toolbar } from '@/components/toolbar/Toolbar';
-import { Formula } from '@/components/formula/Formula';
-import { Table } from '@/components/table/Table';
-import { createStore } from './core/createStore';
-import { rootReducer } from './redux/rootReducer';
-import { debounce, storage } from './core/utils';
-import { initialState } from './redux/initialState';
+import { Router } from './core/routes/Router';
+// import { Excel } from '@/components/excel/Excel';
+// import { Header } from '@/components/header/Header';
+// import { Toolbar } from '@/components/toolbar/Toolbar';
+// import { Formula } from '@/components/formula/Formula';
+// import { Table } from '@/components/table/Table';
+// import { createStore } from './core/createStore';
+// import { rootReducer } from './redux/rootReducer';
+// import { debounce, storage } from './core/utils';
+// import { initialState } from './redux/initialState';
 
-// rootReducer, initialState
-const store = createStore(rootReducer, initialState);
 
-const stateListener = debounce(state => {
-	// console.log('App state: ', state);
-	storage('excel-state', state);
-}, 300);
+new Router('#app', {
 
-store.subscribe(stateListener);
-
-const excel = new Excel('#app', {
-	components: [Header, Toolbar, Formula, Table],
-	store
 });
+// // rootReducer, initialState
+// const store = createStore(rootReducer, initialState);
 
-excel.render();
+// const stateListener = debounce(state => {
+// 	// console.log('App state: ', state);
+// 	storage('excel-state', state);
+// }, 300);
+
+// store.subscribe(stateListener);
+
+// const excel = new Excel('#app', {
+// 	components: [Header, Toolbar, Formula, Table],
+// 	store
+// });
+
+// excel.render();
